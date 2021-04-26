45ca794ddda973ec2663e4edce046c6a422c7522
Tomas Meneses <tomasmeneses22@gmail.com>
2021-04-26 00:37:35 -0300

    add navigation bar

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import Navigator from './Navigation';
+
+export default function App() {
+  return (
+   <NavigationContainer>
+       <Navigator/>
+   </NavigationContainer>
+  );
+}
+
+
